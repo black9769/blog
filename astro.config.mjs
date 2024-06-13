6812be8cbf383209b649817d428f30b1146c8d62
black9769 <black9769@naver.com>
2024-06-14 00:16:21 +0900

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://black9769.github.io/",
-  base: "/",
+  base: "/fuwari",
   trailingSlash: "always",
   integrations: [
     tailwind(),
